03abb4678cb652e638a0928104366f28b42aaadf
luludocteur <luludocteurcrypt@gmail.com>
2024-09-15 18:11:50 +0200

    alors peut etre

--- optimisation_classes.py
+++ optimisation_classes.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.stats import chi2_contingency
 import time
 
-def generate_classes(serie):
+def generate_classes(serie, bins):
     """
     """
 
@@ -20,10 +20,10 @@ def generate_classes(serie):
     def create_test_classes(serie):
         """
         """
-        df_classes = pd.cut(serie, random_cuts(serie, 4), right=True, include_lowest=True)
+        df_classes = pd.cut(serie, random_cuts(serie, bins), right=True, include_lowest=True)
         while any(df_classes.value_counts()<(0.1*len(df_classes))):
 
-            df_classes = pd.cut(serie, random_cuts(serie, 4), right=True, include_lowest=True)
+            df_classes = pd.cut(serie, random_cuts(serie, bins), right=True, include_lowest=True)
 
         return df_classes
         
@@ -32,7 +32,7 @@ def generate_classes(serie):
 
 
 
-def optimize_classes(serie, df_income):
+def optimize_classes(serie, df_income, bins):
     """
     """
     def khi2(df_classes, df_income):
@@ -42,20 +42,21 @@ def optimize_classes(serie, df_income):
         khi2, pval, ddl, contigent_theo = chi2_contingency(table)
         return khi2, pval, ddl
     
-    df_classes_min = generate_classes(serie)
+    df_classes_min = generate_classes(serie, bins)
     khisq_min, pval_min, ddl_min = khi2(df_classes_min, df_income)
     n=100
     deltas=[]
 
     for i in range(n):
         time1 = time.time()*1000
-        df_classes = generate_classes(serie)
+        df_classes = generate_classes(serie, bins)
         time2 = time.time()*1000
         deltas.append((time2-time1)/1000)
-        print(i,'  Temps:', (deltas[-1]))
+        # print(i,'  Temps:', (deltas[-1]))
         khisq, pval, ddl = khi2(df_classes, df_income)
         if (khisq > khisq_min) and (pval<=pval_min):
             conv = i
+            print(conv)
             df_classes_min = df_classes
             khisq_min = khisq
             pval_min = pval
@@ -65,14 +66,28 @@ def optimize_classes(serie, df_income):
     print('Temps moyen pour une génération de table:', sum_delta/n)
         
     return df_classes_min, khisq_min, pval_min, ddl_min, conv
+
+def test(khisq, pval, df_income):
+    df0 = pd.read_csv('files/clean.csv')
+    df1 = pd.read_csv('files/classes_opti_age.csv')
+    serie_originale = df0['age']
+    serie_classe_actuelle = df1['age']
+    table_actuelle = pd.crosstab(serie_classe_actuelle, df_income)
+    khi_act, pval_act, ddl, contigent_theo = chi2_contingency(table_actuelle)
+    if (khisq > khi_act) and (pval<=pval_act):
+        df = pd.concat([serie_classe_actuelle,serie_originale], axis=1)
+        df.to_csv(f'files/classes_opti_{serie_classe_actuelle.name}.csv', columns=['classe_age', 'age_quanti'], index=False)
+        return True
+    else:
+        return False
+
         
 def main():
     df = pd.read_csv('files/clean.csv')
-    print(generate_classes(df['age']))
-    # df_income = df['income']
-    # df_classes, khisq, pval, ddl, conv = optimize_classes(df['hours-per-week'], df_income)
-    # print(df_classes, khisq, pval, ddl, conv)
-    # df_classes.to_csv(f'files/classes_opti_{df_classes.name}.csv', index=False)
+    df_income = df['income']
+    df_classes, khisq, pval, ddl, conv = optimize_classes(df['age'], df_income, 5)
+    print(df_classes, khisq, pval, ddl, conv)
+    print(test(khisq, pval, df_income))
 
 
 if __name__ == "__main__":
