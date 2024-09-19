815c37d1d6ded7dd890dcd0bbe953fae1ab1b86d
luludocteur <luludocteurcrypt@gmail.com>
2024-09-19 10:38:48 +0200

    Continuation Arbre Décision

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('filename')
 args = parser.parse_args()
 
-def Create_Train_Test(df, is_x_dummies):
+def Create_Train_Test(df):
     """
     Créer les dataframes de test et d'entraînement 
     """
@@ -45,32 +45,43 @@ def DecisionTree(X_train, y_train):
     """
     Créer l'arbre de décision grâce aux datasets d'entraînement
     """
-    model = tree.DecisionTreeClassifier(random_state=0).fit(X_train, y_train)
+    model = tree.DecisionTreeClassifier(max_leaf_nodes=10,
+                                        min_samples_leaf=10,
+                                        min_samples_split=30).fit(X_train, y_train)
     return model
 
-def EvaluationLogit(model, X_test, y_test):
+def Evaluation(model, X_test, y_test, isLogit):
     """
     Affiche les résultats du modèle avec les datasets de test
     """
+    file = args.filename.split('.')[0].split('/')[-1]
     predict_Y = model.predict(X_test)
-    print('Intercept', model.intercept_)
-    df_coefficents = pd.DataFrame({'Variables':model.feature_names_in_,
-                                   'Coefficients':model.coef_[0],
-                                   'Odd-Ratios':np.exp(model.coef_[0])})
-    print(df_coefficents.to_string())
+    names = model.feature_names_in_
+    if isLogit:
+        print('Intercept', model.intercept_)
+        df_coefficents = pd.DataFrame({'Variables':names,
+                                    'Coefficients':model.coef_[0],
+                                    'Odd-Ratios':np.exp(model.coef_[0])})
+        print(df_coefficents.to_string())
+    else:
+        df_importance = pd.DataFrame({'Variables':names,
+                                      'Importance':model.feature_importances_}).sort_values(by='Importance',
+                                                                                            ascending=False, ignore_index=True)
+        print(df_importance.loc[df_importance['Importance']!=0])
+        tree.plot_tree(model, feature_names= list(X_test.columns) , filled=True)
+        plt.savefig(f'images/Tree_{file}.pdf')
     Accuracy = model.score(X_test,y_test)
     class_report = classification_report(y_test, predict_Y, output_dict=True)
     class_report = pd.DataFrame(class_report)
-    print(class_report)
     f1_score = class_report.loc['f1-score']['True']
     MCC = matthews_corrcoef(y_test, predict_Y)
-    print('Matrice de confusion :\n', confusion_matrix(y_test, predict_Y))
-    AUC = ROC(model, X_test, y_test, isLogit=True)
+    AUC = ROC(model, X_test, y_test, isLogit)
 
     df_metrics = pd.DataFrame({'Valeur':[AUC, f1_score, Accuracy, MCC],
                                'Cible':[0.8, 0.75, 0.85, 0.75]},
                                index = ['AUC', 'F1-Score', 'Accuracy', 'MCC'])
     return df_metrics
+
 
 def ROC(model, X_test, y_test, isLogit):
     """
@@ -83,7 +94,7 @@ def ROC(model, X_test, y_test, isLogit):
     FER, TER, threshold = roc_curve(y_test, model.predict_proba(X_test)[:,1])
     AUC = auc(FER,TER)
 
-    ### ROC
+    ### ROC ####
     plt.figure()
     plt.plot(FER, TER)
     plt.annotate(f'AUC:{round(AUC, 2)}', (0.7,0.3))
@@ -94,50 +105,44 @@ def ROC(model, X_test, y_test, isLogit):
 
     return AUC
 
-def Scoring(df_metrics):
+def Scoring(df_metrics, isLogit):
     """
     
     """
-    print("#### SCORING ####")
-    print(df_metrics)
-    print('AUC:', df_metrics.loc['AUC']['Valeur'])
-    print('f1_score:', df_metrics.loc['F1-Score']['Valeur'])
-    print('Accuracy:', df_metrics.loc['Accuracy']['Valeur'])
-    print('MCC:', df_metrics.loc['MCC']['Valeur'])
+    if isLogit:
+        model = 'Logit'
+    else:
+        model = 'Tree'
 
+    print(f"#### SCORING {model} ####")
+    print(df_metrics)
     score = df_metrics.apply(lambda row : 1 if row['Valeur'] > row['Cible'] else 0, axis=1).sum()
-    print('Score du Logit:', score)
+    print(f'Score du {model}:', score)
 
 def LOGIT(df):
-    #### LOGIT ####
-    X_train, X_test, y_train, y_test = Create_Train_Test(df, True)
+    """
+    """
+    X_train, X_test, y_train, y_test = Create_Train_Test(df)
     vars = ['native-country_United-States', 'workclass_Private', 'occupation_Prof-specialty', 
             'gender_Male', 'education_HS-grad', 'relationship_Husband', 'marital-status_Married-civ-spouse',
             'race_White']
     refs_vars, X_train, X_test = Drop_References_Variables(X_train, X_test, vars)
     model_logit = Logistic_Regression(X_train, y_train)
-    df_metrics_logit = Evaluation(model_logit, X_test, y_test)
-    Scoring(df_metrics_logit)
-    ###############
+    df_metrics_logit = Evaluation(model_logit, X_test, y_test, True)
+    Scoring(df_metrics_logit, True)
 
 def TREE(df):
-    #### TREE #####
-    X_train, X_test, y_train, y_test = Create_Train_Test(df, False)
+    """
+    """
+    X_train, X_test, y_train, y_test = Create_Train_Test(df)
     model_decision_tree = DecisionTree(X_train, y_train)
-    df_metrics_tree = Evaluation(model_decision_tree, X_test, y_test)
-    Scoring(df_metrics_tree)
-    ###############
+    df_metrics_tree = Evaluation(model_decision_tree, X_test, y_test, False)
+    Scoring(df_metrics_tree, False)
 
 def main():
 
     df = pd.read_csv(args.filename)
     TREE(df)
-
-
-
-    
-
-
 
 if __name__ == "__main__":
     main()
